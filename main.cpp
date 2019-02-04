0892601c66066a11d766a81fe211d2c4d62afb82
CG Bahk <cg.bahk@gmail.com>
2019-02-04 14:20:34 +0900

    Initial commit for problem for 1096-E
    https://codeforces.com/contest/1096/problem/E
    RangedSum, Mod and combi implemented. RangedSum would be changed.

---
+++ main.cpp
@@ -0,0 +1,247 @@
+#define LOCAL_TEST
+#include <iostream>
+#include <cassert>
+typedef long long ll;
+using namespace std;
+
+const static int PL = 100, S = 50;
+const static int P = 998244353;
+
+struct RngdSum
+{
+  /// Initialize memo to -1, i.e. not visited(calculated)
+  // TODO delete rng and make 2D dp
+  RngdSum()
+  {
+    for (int len=0; len<PL; len++)
+    {
+      for (int rng=0; rng<S; rng++)
+      {
+        for (int sum=0; sum<S; sum++)
+        {
+          _memo[len][rng][sum] = -1;
+        }
+      }
+    }
+  }
+
+  /**
+   * calculate # of combination of sum of ranged integers
+   * 
+   * Returns # of possible RngdSumnations of a[] hold following:
+   * a[1] + a[2] + ... + a[len] = sum where a[i] in [0, rng]
+   */
+  int calc(int len, int rng, int sum)
+  {
+    if(_memo[len][rng][sum] != -1)
+    {
+      return _memo[len][rng][sum];
+    }
+
+    if(len * rng < sum)
+    {
+      _memo[len][rng][sum] = 0;
+      return 0;
+    }
+
+    if(len == 1)
+    {
+      if(rng >= sum)
+      {
+        _memo[len][rng][sum] = 1;
+        return 1;
+      }
+      else
+      {
+        _memo[len][rng][sum] = 0;
+        return 0;
+      }
+    }
+
+    // TODO may remove, as may redundant
+    if(sum < 0)
+    {
+      _memo[len][rng][sum] = 0;
+      return 0;
+    }
+
+    if(sum == 0)
+    {
+      _memo[len][rng][sum] = 1;
+      return 1;
+    }
+
+    int val = 0;
+    // this method has faster implementation by delete overlap
+    for(int k=0; k<=rng && sum-k>=0; k++)
+    {
+      val += calc(len-1, rng, sum-k);
+    }
+    _memo[len][rng][sum] = val;
+    return val;
+  }
+
+private:
+  int _memo[PL][S][S];
+};
+
+/// class for Modulo P operation
+struct Mod
+{
+  Mod(int val)
+  {
+    _val = val % P;
+  }
+
+  Mod(ll val)
+  {
+    _val = (int) ( val % (ll)P );
+  }
+
+  int val(){ return _val; }
+
+  Mod operator+(Mod rhs)
+  {
+    int temp = this->val() + rhs.val();
+    temp %= P;
+    return Mod(temp);
+  }
+
+  Mod operator-(Mod rhs)
+  {
+    int temp = this->val() - rhs.val() + P;
+    temp %= P;
+    return Mod(temp);
+  }
+
+  Mod operator*(Mod rhs)
+  {
+    ll temp = ( (ll)(this->val()) * (ll)(rhs.val()) ) % (ll)P;
+    return Mod(temp);
+  }
+
+  Mod operator/(Mod rhs)
+  {
+    return (*this) * inverse(rhs);
+  }
+
+  bool operator==(Mod rhs)
+  {
+    return *this == rhs.val();
+  }
+
+  bool operator==(int rhs)
+  {
+    return (this->val() == rhs) ? true : false;
+  }
+
+private:
+  Mod inverse(Mod arg)
+  {
+    assert( arg.val() != 0 );
+    return power(arg, P-2);
+  }
+
+  Mod power(Mod arg, int pow)
+  {
+    if(pow == 1){ return arg; }
+    if(pow % 2)
+    { 
+      Mod temp = power(arg, pow/2);
+      return temp * temp * arg;
+    }
+    else
+    { 
+      Mod temp = power(arg, pow/2);
+      return temp * temp;
+    }
+  }
+
+  int _val;
+};
+
+/// calculate (n choose k) in Mod
+Mod combi(int n, int k)
+{
+  if(n < 2*k){ return combi(n, n-k); }
+
+  Mod ret(1);
+  for(int i=0; i<k; i++)
+  {
+    ret = ret * Mod(n-i);
+    ret = ret / Mod(i+1);
+  }
+  return ret;
+}
+
+void test()
+{
+  // RngdSum test
+  {
+    RngdSum cb;
+
+    assert(cb.calc(2, 3, 6) == 1);
+    assert(cb.calc(2, 4, 6) == 3);
+    assert(cb.calc(3, 5, 15) == 1);
+    assert(cb.calc(3, 5, 14) == 3);
+    assert(cb.calc(5, 3, 14) == 5);
+  }
+  
+  // Mod test
+  {
+    assert( Mod(P-1) + Mod(P-1) == P-2 );
+    assert( Mod(1) - Mod(2) == P-1 );
+    assert( Mod(0) - Mod(P-1) == 1 );
+    assert( Mod(P-1) * Mod(P-1) == 1 );
+    assert( Mod(P-1) * Mod(P-1) * Mod(P-1) == P-1 );
+    assert( Mod(1) / Mod(P-1) == P-1 );
+
+    assert( Mod(7) / Mod(8) == 124780545);
+    assert( Mod(1)/Mod(2) + Mod(1)/Mod(3) == Mod(5)/Mod(6) );
+    assert( Mod(1)/Mod(2) - Mod(1)/Mod(3) == Mod(1)/Mod(6) );
+    assert( (Mod(1)/Mod(2)) / Mod(3) == Mod(1)/Mod(6) );
+  }
+
+  // combi test
+  {
+    assert( combi(1, 1) == 1 );
+    assert( combi(2, 1) == 2 );
+    assert( combi(6, 2) == 15 );
+    assert( combi(6, 3) == 20 );
+    assert( combi(6, 4) == 15 );
+    combi(5000, 2500); // for speed concern
+  }
+  cout << "Test passed" << endl;
+}
+
+void prob()
+{
+  do
+  {
+    int p, s, r;
+    cin >> p >> s >> r;
+    if( p < 0 ){ break; }
+    
+    // solve problem
+    Mod num(0), den(0);
+    for(int k=0; k<=s-r; k++)
+    {
+      RngdSum rs;
+
+    }
+
+  } while(1);
+}
+
+int main()
+{
+#ifdef LOCAL_TEST
+//  freopen("input.txt", "r", stdin);
+#endif
+
+  test();
+  //prob();
+
+  return 0;
+}
+
